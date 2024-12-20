639dfa1ced28bc83bccf9a76264d9eaaa1a849be
Tilbo Roaim <roaimtilbo6363@gmail.com>
2024-12-20 12:06:42 +0100

    daily reminder

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -14,7 +14,7 @@ match priority:
     case "low":
         reminde = f"'{task}'  is a {priority} priority task."
 if time_bound == "yes":
-    print(f"Reminder : {reminde} that require immediate attention today!")
+    print(f"Reminder: {reminde} that require immediate attention today!")
 
 if time_bound == "no":
-    print(f"Reminder : {reminde} Consider completing it when you have free time.")
+    print(f"Note: {reminde} Consider completing it when you have free time.")
