4ca4198e5d21ba80c8e3b262815b188571a457a5
Tilbo239 <roaimtilbo6363@gmail.com>
2025-01-10 16:31:18 +0100

    shopping list

--- fns_and_dsa/main.py
+++ fns_and_dsa/main.py
@@ -1,14 +1,33 @@
-from arithmetic_operations import perform_operation
+# from arithmetic_operations import perform_operation
+from shopping_list_manager import display_menu, add_item, remove_item, view_list
 
 
 def main():
-    print("Arithmetic Operations")
-    num1 = float(input("Enter the first number: "))
-    num2 = float(input("Enter the second number: "))
-    operation = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
+    # print("Arithmetic Operations")
+    # num1 = float(input("Enter the first number: "))
+    # num2 = float(input("Enter the second number: "))
+    # operation = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
 
-    result = perform_operation(num1, num2, operation)
-    print(f"Result: {result}")
+    # result = perform_operation(num1, num2, operation)
+    # print(f"Result: {result}")
+    
+    shopping_list = []
+    while True:
+        display_menu()
+        choice = input("Enter  your choice: ")
+
+        if choice == '1':
+            add_item(shopping_list)
+        elif choice == '2':
+            remove_item(shopping_list)
+        elif choice == '3':
+            view_list(shopping_list)
+        elif choice == '4':
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please  try again.")
+
 
 if __name__ == "__main__":
     main()
---
+++ fns_and_dsa/shopping_list_manager.py
@@ -0,0 +1,32 @@
+def display_menu():
+    print("Shopping List Manager")
+    print("1. Add Item")
+    print("2. Remove Item")
+    print("3. View List")
+    print("4. Exit")
+
+def add_item(shopping_list):
+    item = input("Enter the item to add: ").strip()
+    if item:
+        shopping_list.append(item)
+        print(f"'{item}' has been added to the card.")
+    else:
+        print("Invalid input. Please enter a valid item.")
+
+def remove_item(shopping_list):
+    item = input("Enter the item to remove: ").strip()
+
+    if item not in shopping_list:
+        print(f"{item} is not in the shopping list")
+    else:
+        shopping_list.remove(item)
+        print(f"{item} has been removed from the  list shopping")
+
+
+def view_list(shopping_list):
+    if not shopping_list:
+        print("The shopping list is empty.")
+    else:
+        for item in shopping_list:
+            print(item)
+    
